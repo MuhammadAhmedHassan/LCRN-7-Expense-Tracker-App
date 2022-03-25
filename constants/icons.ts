7378cbecd2e8021b263ffc9c900729cb8e0f2309
Muhammad Ahmed Hassan <ah363702@gmail.com>
2022-03-25 08:42:16 +0500

    add constants

---
+++ constants/icons.ts
@@ -0,0 +1,31 @@
+const baby_car = require('../assets/icons/baby_car_icon.png');
+const back_arrow = require('../assets/icons/back_arrow_icon.png');
+const calendar = require('../assets/icons/calendar_icon.png');
+const chart = require('../assets/icons/chart_icon.png');
+const cloth_icon = require('../assets/icons/cloth_icon.png');
+const down_arrow = require('../assets/icons/down_arrow.png');
+const education = require('../assets/icons/education_icon.png');
+const food = require('../assets/icons/food_icon.png');
+const healthcare = require('../assets/icons/healthcare_icon.png');
+const menu = require('../assets/icons/menu_icon.png');
+const more = require('../assets/icons/more_icon.png');
+const pin = require('../assets/icons/pin.png');
+const sports_icon = require('../assets/icons/sports_icon.png');
+const up_arrow = require('../assets/icons/up_arrow.png');
+
+export default {
+  baby_car,
+  back_arrow,
+  calendar,
+  chart,
+  cloth_icon,
+  down_arrow,
+  education,
+  food,
+  healthcare,
+  menu,
+  more,
+  pin,
+  sports_icon,
+  up_arrow,
+};
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+import icons from './icons';
+import images from './images';
+import theme from './theme';
+
+const {COLORS, SIZES, FONTS} = theme;
+
+export {icons, images, theme, COLORS, SIZES, FONTS};
